0138f34c922dbba6134cc4946e00410125b0a3f9
kleranscoding <clarencecc88@gmail.com>
2019-04-14 16:20:59 -0500

    2nd attempt keylock modal (set css left/right)

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,34 @@
 function sliderAnimation(event) {
-  var containerWidth = $('.slide-btn-container').width();
-  console.log($(event.target).position().left);
   
-  if ($(event.target).position().left < containerWidth/2.0) {
-    
-    $('.slide-container-msg').html($('.slide-container-msg').html().replace('unlock','lock'));
-    $('.slide-btn').animate({
-      'left': parseFloat(containerWidth-$(event.target).width()-2)+'px' 
-    });
-    console.log($(event.target).position());
+  var containerWidth = $('.slide-btn-container').width();
+  var paddingWidth = 30-4;
+  var toSlidePx = containerWidth-$(event.target).width()-2;
+  var animateTime = 100; 
+
+  // check the slider button horizontal position
+  if ($(event.target).position().left < 0.5 * containerWidth) {
+    // animate the button to slide to right
+    $('.slide-btn')
+    .css({'right': paddingWidth+'px', 'left': ''});
+    //.animate({ 'left': '+='+toSlidePx+'px' }, animateTime);
+    // change background-color
     $('.slide-btn-container').css(
       'backgroundColor', 'rgba(76,149,153,0.2)'
     );
-    
+    // change message
+    $('.slide-container-msg').html($('.slide-container-msg').html().replace('unlock','lock'));
+    $('.slide-btn-msg').html('&#x2714;').css('color', '#4c9599');
 
   } else {
     
-    $('.slide-container-msg').html($('.slide-container-msg').html().replace('lock','unlock'));
-    $('.slide-btn').animate({
-      'left': '-3px' 
-    });
+    $('.slide-btn')
+    .css({'left': paddingWidth+'px', 'right': ''});
+    //.animate({ 'left': '-='+toSlidePx+'px' }, animateTime);
     $('.slide-btn-container').css(
       'backgroundColor', 'white'
     );
-    
-    console.log($(event.target).position());
+    $('.slide-container-msg').html($('.slide-container-msg').html().replace('lock','unlock'));
+    $('.slide-btn-msg').html('&rAarr;').css('color', '#484848');
   }
   
 }
