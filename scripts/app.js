971ec93d454a85669254b0945d3d31aee695a340
kleranscoding <clarencecc88@gmail.com>
2019-04-14 18:11:04 -0500

    3rd attempt on key_lock modal button

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,26 @@
 function sliderAnimation(event) {
   
   var containerWidth = $('.slide-btn-container').width();
-  var paddingWidth = 30-4;
-  var toSlidePx = containerWidth-$(event.target).width()-2;
-  var animateTime = 100; 
+  var paddingWidth = 0;
 
+  console.log($(event.target).position().left);
   // check the slider button horizontal position
-  if ($(event.target).position().left < 0.5 * containerWidth) {
+  if ($(event.target).position().left <  0.5*containerWidth) {
     // animate the button to slide to right
-    $('.slide-btn')
-    .css({'right': paddingWidth+'px', 'left': ''});
-    //.animate({ 'left': '+='+toSlidePx+'px' }, animateTime);
+    $('.slide-btn').attr('src', 'images/locked.png')
+      .css({'right': paddingWidth+'px', 'left': ''});
+    
     // change background-color
-    $('.slide-btn-container').css(
-      'backgroundColor', 'rgba(76,149,153,0.2)'
-    );
+    $('.slide-btn-container').css( 'backgroundColor', 'rgba(76,149,153,0.2)');
+    
     // change message
     $('.slide-container-msg').html($('.slide-container-msg').html().replace('unlock','lock'));
     $('.slide-btn-msg').html('&#x2714;').css('color', '#4c9599');
 
   } else {
     
-    $('.slide-btn')
-    .css({'left': paddingWidth+'px', 'right': ''});
-    //.animate({ 'left': '-='+toSlidePx+'px' }, animateTime);
+    $('.slide-btn').attr('src', 'images/unlocked.png')
+      .css({'left': paddingWidth+'px', 'right': ''});
     $('.slide-btn-container').css(
       'backgroundColor', 'white'
     );
