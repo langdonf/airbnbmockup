d615d2a4a26bf3cbbc63f35d36865fb650cb2374
kleranscoding <clarencecc88@gmail.com>
2019-04-15 12:58:03 -0500

    wrapped app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,41 @@
-function sliderAnimation(event) {
-  
-  // check the slider button horizontal position
-  if ($(event.target).position().left <  0.5*$('.slide-btn-container').width()) {
-    
-    // animate the button to slide to right
-    $('.slide-btn').attr('src', 'images/locked.png')
-      .css({'right': '0px', 'left': ''});
-    
-    // change background-color
-    $('.slide-btn-container').css( 'backgroundColor', 'rgba(76,149,153,0.2)');
-    
-    // change message
-    $('.slide-container-msg').html($('.slide-container-msg').html().replace('unlock','lock'));
-    $('.slide-btn-msg').html('&#x2714;').css('color', '#4c9599');
+(function ($) {
 
-  } else {
+  function sliderAnimation(event) {
     
-    $('.slide-btn').attr('src', 'images/unlocked.png')
-      .css({'left': '0px', 'right': ''});
-    $('.slide-btn-container').css('backgroundColor', 'white');
-    $('.slide-container-msg').html($('.slide-container-msg').html().replace('lock','unlock'));
-    $('.slide-btn-msg').html('&rAarr;').css('color', '#484848');
-  }
-  
-}
+    // check the slider button horizontal position
+    if ($(event.target).position().left <  0.5*$('.slide-btn-container').width()) {
+      
+      // animate the button to slide to right
+      $('.slide-btn').attr('src', 'images/locked.png')
+        .css({'right': '0px', 'left': ''});
+      
+      // change background-color
+      $('.slide-btn-container').css( 'backgroundColor', 'rgba(76,149,153,0.2)');
+      
+      // change message
+      $('.slide-container-msg').html($('.slide-container-msg').html().replace('unlock','lock'));
+      $('.slide-btn-msg').css('color', '#4c9599');
 
-$(document).ready(function(){
-
-  $('.modal').modal();
-
-  $('.slide-btn').swipe({'swipe': function(event) {
-      sliderAnimation(event);
+    } else {
+      
+      $('.slide-btn').attr('src', 'images/unlocked.png')
+        .css({'left': '0px', 'right': ''});
+      $('.slide-btn-container').css('backgroundColor', 'white');
+      $('.slide-container-msg').html($('.slide-container-msg').html().replace('lock','unlock'));
+      $('.slide-btn-msg').css('color', '#484848');
     }
+    
+  }
+
+  $(document).ready(function(){
+
+    $('.modal').modal();
+
+    $('.slide-btn').swipe({'swipe': function(event) {
+        sliderAnimation(event);
+      }
+    });
+    
   });
-  
-});
+
+})(jQuery);
