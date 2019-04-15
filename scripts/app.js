a11f212191800c970999ed7134ab13b8e047760c
kleranscoding <clarencecc88@gmail.com>
2019-04-14 22:41:30 -0500

    swipe to lock and unlock using touchSwipe plugin

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,10 @@
 function sliderAnimation(event) {
   
-  var containerWidth = $('.slide-btn-container').width();
-  var paddingWidth = 0;
-
-  console.log($(event.target).position().left);
   // check the slider button horizontal position
-  if ($(event.target).position().left <  0.5*containerWidth) {
+  if ($(event.target).position().left <  0.5*$('.slide-btn-container').width()) {
     // animate the button to slide to right
     $('.slide-btn').attr('src', 'images/locked.png')
-      .css({'right': paddingWidth+'px', 'left': ''});
+      .css({'right': '0px', 'left': ''});
     
     // change background-color
     $('.slide-btn-container').css( 'backgroundColor', 'rgba(76,149,153,0.2)');
@@ -20,10 +16,8 @@ function sliderAnimation(event) {
   } else {
     
     $('.slide-btn').attr('src', 'images/unlocked.png')
-      .css({'left': paddingWidth+'px', 'right': ''});
-    $('.slide-btn-container').css(
-      'backgroundColor', 'white'
-    );
+      .css({'left': '0px', 'right': ''});
+    $('.slide-btn-container').css('backgroundColor', 'white');
     $('.slide-container-msg').html($('.slide-container-msg').html().replace('lock','unlock'));
     $('.slide-btn-msg').html('&rAarr;').css('color', '#484848');
   }
@@ -34,7 +28,8 @@ $(document).ready(function(){
 
   $('.modal').modal();
 
-  $('.slide-btn').click(function(event) {
-    sliderAnimation(event);
+  $('.slide-btn').swipe({'swipe': function(event) {
+      sliderAnimation(event);
+    }
   });
 });
